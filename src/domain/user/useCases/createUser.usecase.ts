b9a30104a0f1013ff6eeb205ba8ddf5e38ff119a
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-03-21 03:07:47 -0300

    refactor: remove usecase files

--- src/domain/user/useCases/createUser.usecase.ts
+++
@@ -1,6 +0,0 @@
-import { User } from "@prisma/client";
-import { CreateUserDto } from "../dtos/createUser.dto";
-
-export interface ICreateUserUseCase {
-  execute(user: CreateUserDto): Promise<User>;
-}
--- src/domain/user/useCases/deactivateUser.usecase.ts
+++
@@ -1,3 +0,0 @@
-export interface IDeactivateUserUseCase {
-  execute(id: string): Promise<string>;
-}
