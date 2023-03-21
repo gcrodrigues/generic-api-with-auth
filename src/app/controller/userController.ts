32f29d78097d2234071288179f708822164ca932
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-03-21 00:29:53 -0300

    refactor: add try catch

--- src/app/controller/userController.ts
+++ src/app/controller/userController.ts
@@ -6,16 +6,24 @@ import { DeactivateUserUseCase } from '../useCases/deactivateUser.usecase';
 
 export class UserController {
   async createUser(req: Request, res: Response): Promise<Response> {
-      const { name, email } = req.body;
-      const createUser = container.resolve(CreateUserUseCase);
+    const { name, email } = req.body;
+    const createUser = container.resolve(CreateUserUseCase);
+    try {
       const user = await createUser.execute({ name, email }); 
       return res.status(200).json(user)
+    } catch (e) {
+      return res.status(400).json(e)
+    }
   }
 
   async deactivateUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.body;
     const deactivateUser = container.resolve(DeactivateUserUseCase);
-    const deactivatedUser = await deactivateUser.execute(id); 
-    return res.status(200).json({id: deactivatedUser.id})
+    try {
+      const deactivatedUser = await deactivateUser.execute(id); 
+      return res.status(200).json({id: deactivatedUser.id})
+    } catch (e) {
+      return res.status(400).json(e)
+    }
   }
 }
