f0bd2966136f0177dcafcbec8fca0448148e5801
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-03-21 01:38:04 -0300

    feat: add cors

--- src/interface/http/server.ts
+++ src/interface/http/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata'
 import '../../config/container'
 import express from 'express';
+import cors from 'cors'
 
 import routes  from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
