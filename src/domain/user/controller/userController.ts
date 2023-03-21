e9385ba11b505561e37db821e06a4d3a1de0a952
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-03-21 03:12:49 -0300

    refactor: remove try catch blocks

--- src/domain/user/controller/userController.ts
+++ src/domain/user/controller/userController.ts
@@ -8,22 +8,14 @@ export class UserController {
   async createUser(req: Request, res: Response): Promise<Response> {
     const { name, email, password } = req.body;
     const createUser = container.resolve(CreateUserService);
-    try {
-      const user = await createUser.execute({ name, email, password }); 
-      return res.status(200).json(user)
-    } catch (e) {
-      return res.status(400).json(e)
-    }
+    const user = await createUser.execute({ name, email, password }); 
+    return res.status(200).json(user)
   }
 
   async deactivateUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.body;
     const deactivateUser = container.resolve(DeactivateUserService);
-    try {
-      const deactivatedUser = await deactivateUser.execute(id); 
-      return res.status(200).json({id: deactivatedUser.id})
-    } catch (e) {
-      return res.status(400).json(e)
-    }
+    const deactivatedUser = await deactivateUser.execute(id); 
+    return res.status(200).json({id: deactivatedUser.id})
   }
 }
