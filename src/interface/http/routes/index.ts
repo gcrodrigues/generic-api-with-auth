06846b6637150bcf0380e780aebb96e046fb9217
Gustavo Carvalho <gcrodrigues413@gmail.com>
2023-03-25 02:42:09 -0300

    feat: create profile routes

--- src/interface/http/routes/index.ts
+++ src/interface/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import profileRouter from '../../../domain/user/interface/http/routes/profile.routes';
+import usersRouter from '../../../domain/user/interface/http/routes/user.routes';
 import authRouter from './auth.routes';
-import usersRouter from './user.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/profile', profileRouter);
 routes.use('/auth', authRouter);
 
 export default routes;
